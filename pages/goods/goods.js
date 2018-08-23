9971c34dce503d5d3b8e32b32d684e1cb7cd082b
hez <626815494@qq.com>
2018-08-23 15:14:17 +0800

    商品详细

--- pages/goods/goods.js
+++ pages/goods/goods.js
@@ -1,3 +1,4 @@
+
 // pages/goods/goods.js
 Page({
 
@@ -5,14 +6,67 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    maps:false,
+    int:true,
+    latitude:"",
+    longitude:""
+  },
+
+  // 显示位置
+  onMap: function(){
+    wx.showLoading({
+      title: '地图加载中',
+    })
+    this.setData({
+      int: false
+    })
+    let that = this
+    wx.getLocation({
+      type: 'wgs84',
+      success: function (res) {
+        var latitude = res.latitude
+        var longitude = res.longitude
+        var speed = res.speed
+        var accuracy = res.accuracy
+        console.log(res)
+        wx.hideLoading()
+        that.setData({
+          maps:true,
+          int: false,
+          latitude: res.latitude,
+          longitude: res.longitude
+        })
+      }
+    })
+  },
+
+  // 关闭地图
+  offMap: function(){
+    this.setData({
+      maps: false
+    })
+  },
+
+  // 打开积分
+  onInt: function(){
+    this.setData({
+      int: true,
+      maps: false
+    })
+  },
+
+  // 关闭积分
+  offInt: function(){
+    this.setData({
+      int:false
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    
   },
 
   /**
