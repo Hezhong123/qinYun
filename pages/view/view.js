7f7744d6e77697af9e29fb7c5de4c60942010ebf
hez <626815494@qq.com>
2018-09-06 17:36:11 +0800

    描述修改

--- pages/view/view.js
+++ pages/view/view.js
@@ -1,5 +1,8 @@
 // pages/view/view.js
 const app = getApp()
+const wxParser = require('../../wxParser/index');
+import { getGoodsCent } from '../../utils/api.js'
+
 Page({
 
   /**
@@ -13,9 +16,24 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      texts: app.globalData.texts
-    })
+    getGoodsCent(res => {
+      wx.setNavigationBarTitle({
+        title: res.title
+      })
+      wxParser.parse({
+        bind: 'richText',
+        html: res.content,
+        target: this,
+        enablePreviewImage: false, // 禁用图片预览功能
+        tapLink: (url) => { // 点击超链接时的回调函数
+          // url 就是 HTML 富文本中 a 标签的 href 属性值
+          // 这里可以自定义点击事件逻辑，比如页面跳转
+          wx.navigateTo({
+            url
+          });
+        }
+      });
+    }, { richTextID: 1536222829394408 })
   },
 
   /**
